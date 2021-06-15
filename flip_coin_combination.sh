00bb76e0df852658af68eff9c14c2878bb287681
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-06-15 21:14:51 +0530

    Single combination with percentage display

--- flip_coin_combination.sh
+++ flip_coin_combination.sh
@@ -1,11 +1,36 @@
 #!/bin/bash -x
 echo "Welcome to flip coin combination problem"
 
-toss=$((RANDOM%2))
+read -p "Enter number of times to toss the coin: " num
 
-if [ $toss -eq 0 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A singlet_combination=(
+					[H]=0
+					[T]=0
+				)
+# Singlet Combination
+
+for (( flip=1; flip<=num; flip++ ))
+do
+
+	toss=$((RANDOM%2))
+
+	case $toss in
+
+	0)
+		echo "Heads"
+		(( singlet_combination[H]++ ))
+			;;
+
+	1)
+		echo "Tails"
+		(( singlet_combination[T]++ ))
+			;;
+esac
+done
+
+for key in ${!singlet_combination[@]}
+do
+	percentage=$((${singlet_combination[$key]} *100 / $num))
+	singlet_combination[$key]=$percentage
+	echo "Percentage of $key is ${singlet_combination[$key]}"
+done
