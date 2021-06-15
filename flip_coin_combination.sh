4e9136f9598daadb1ba4691025c22e3a1be7634c
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-06-15 21:17:26 +0530

    doublet combination with percentage display

--- flip_coin_combination.sh
+++ flip_coin_combination.sh
@@ -34,3 +34,47 @@ do
 	singlet_combination[$key]=$percentage
 	echo "Percentage of $key is ${singlet_combination[$key]}"
 done
+
+echo "***************Doublet Combination******************"
+
+declare -A doublet_combination=(
+					[HH]=0
+					[HT]=0
+					[TT]=0
+					[TH]=0
+				)
+
+for (( flip=1; flip<=num; flip++ ))
+do
+	toss_1=$((RANDOM%2))
+	toss_2=$((RANDOM%2))
+
+	case $toss_1$toss_2 in
+
+	00)
+		echo "Heads Heads"
+		(( doublet_combination[HH]++ ))
+			;;
+
+	01)
+		 echo "Heads Tails"
+                (( doublet_combination[HT]++ ))
+                        ;;
+	11)
+                 echo "Tails Tails"
+                (( doublet_combination[TT]++ ))
+                        ;;
+	10)
+                 echo "Tails Heads"
+                (( doublet_combination[TH]++ ))
+                        ;;
+esac
+done
+echo ${doublet_combination[@]}
+
+for key in ${!doublet_combination[@]}
+do
+	percentage=$((${doublet_combination[$key]}*100/$num))
+	doublet_combination[$key]=$percentage
+	echo "Percentage of $key is ${doublet_combination[$key]}"
+done
