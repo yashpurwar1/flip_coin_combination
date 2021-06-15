bb0b2b18c9804c7faeb8b75140a82914a46101c6
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-06-15 21:22:00 +0530

    triplet combination with percentage display

--- flip_coin_combination.sh
+++ flip_coin_combination.sh
@@ -30,9 +30,10 @@ done
 
 for key in ${!singlet_combination[@]}
 do
+	#update count values  with corresponding percentage
 	percentage=$((${singlet_combination[$key]} *100 / $num))
 	singlet_combination[$key]=$percentage
-	echo "Percentage of $key is ${singlet_combination[$key]}"
+	echo "Percentage of $key is ${singlet_combination[$key]%}"
 done
 
 echo "***************Doublet Combination******************"
@@ -70,11 +71,77 @@ do
                         ;;
 esac
 done
-echo ${doublet_combination[@]}
 
 for key in ${!doublet_combination[@]}
 do
+	#update count values  with corresponding percentage
 	percentage=$((${doublet_combination[$key]}*100/$num))
 	doublet_combination[$key]=$percentage
-	echo "Percentage of $key is ${doublet_combination[$key]}"
+	echo "Percentage of $key is ${doublet_combination[$key]%}"
+done
+
+
+
+echo "******************Triplet Combination*******************"
+declare -A triplet_combination=(
+                                    [HHH]=0
+                                    [HHT]=0
+                                    [HTH]=0
+                                    [HTT]=0
+                                    [THH]=0
+                                    [THT]=0
+                                    [TTH]=0
+                                    [TTT]=0
+                                )
+
+for(( flip = 1; flip <= num; flip++ ))
+do
+    ((toss_1=RANDOM%2))
+    ((toss_2=RANDOM%2))
+    ((toss_3=RANDOM%2))
+
+    #0 is mapped as heads
+    #1 is mappes as tails
+    case $toss_1$toss_2$toss_3 in
+
+        000)
+            	echo "Heads Heads Heads"
+            	(( triplet_combination[HHH]++ )) 
+            		;;
+        001)
+            	echo "Heads Heads Tails"
+            	(( triplet_combination[HHT]++ ))
+            		;;
+        010)
+            	echo "Heads Tails Heads"
+            	(( triplet_combination[HTH]++ ))
+            		;;
+        011)
+            	echo "Heads Tails Tails"
+            	(( triplet_combination[HTT]++ ))
+            		;;
+        100)
+            	echo "Tails Heads Heads"
+            	(( triplet_combination[THH]++ )) 
+            		;;
+        101)
+            	echo "Tails Heads Tails"
+            	(( triplet_combination[THT]++ ))
+            		;;
+        110)
+            	echo "Tails Tails Heads"
+            	(( triplet_combination[TTH]++ ))
+            		;;
+        111)
+           	echo "Tails Tails Tails"
+            	(( triplet_combination[TTT]++ ))
+            		;;
+    esac
+done
+for key in ${!triplet_combination[@]}
+do
+	#update count values  with corresponding percentage
+        percentage=$(( ${triplet_combination[$key]}*100/$num ))
+    	triplet_combination[$key]=$percentage
+    	echo "percentage of $key is ${triplet_combination[$key]}%"
 done
